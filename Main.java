732a6ca04d65be468c53d83da0a9c2a24455e3f3
theo-art-code <theodorehmeier@gmail.com>
2026-04-13 23:31:52 +0000

    Added the main program, which gives random answers

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>();
+
+        strings.add("Yes");
+        strings.add("No");
+        strings.add("Maybe");
+        strings.add("You can count on it!");
+        strings.add("Definitely not!");
+        strings.add("Unclear. Try again later.");
+
+        Random random = new Random();
+        String randomString = strings.get(random.nextInt(strings.size()));
+        System.out.println(randomString);
+    }
+}
